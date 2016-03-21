aebb6b7957a6ca0afb531b7e869757d3c16b90b8
kel85uk <kel85uk@gmail.com>
2016-03-21 23:16:20 +0000

    Modifications to home_auto

--- home_auto/test_script.py
+++ home_auto/test_script.py
@@ -9,14 +9,23 @@ o.lat = '52.0167'
 o.long = '4.3667'
 s = ephem.Sun()
 s.compute()
+UTC_offset = 1 # Can always switch to daylight check
+sleep_hour = 23
+wake_hour = 7
 
 
 while True:
-	if (os.system("ping -c 1 192.168.1.4")==0)and(time.gmtime().tm_hour>ephem.localtime(o.next_setting(s)).hour)and(time.gmtime().tm_hour<23):
+	current_hour = time.gmtime().tm_hour + UTC_offset
+	sunset_hour = ephem.localtime(o.next_setting(s)).hour + UTC_offset
+	daylight_hour = (ephem.localtime(o.next_rising(s)).hour + 1) + UTC_offset # Give an extra hour for daylight
+	condition1 = (os.system("ping -c 1 192.168.1.4")==0)
+	condition2 = (current_hour>sunset_hour)or(current_hour<sleep_hour)
+	condition3 = (current_hour>wake_hour)or(current_hour<daylight_hour)
+	if condition1 and (condition2 or condition3):
 		print "Switch on"
-		en.switch_on()
+		#en.switch_on()
 	else:
 		print "Switch off"
-		en.switch_off()
+		#en.switch_off()
 	time.sleep(30)
 	s.compute()
