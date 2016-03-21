b1c2c120cea4451cc421c47f4c3c419245e5ac1e
kel85uk <kel85uk@gmail.com>
2016-03-21 23:25:16 +0000

    Bug fix conditionals for home_auto

--- home_auto/test_script.py
+++ home_auto/test_script.py
@@ -17,10 +17,10 @@ wake_hour = 7
 while True:
 	current_hour = time.gmtime().tm_hour + UTC_offset
 	sunset_hour = ephem.localtime(o.next_setting(s)).hour + UTC_offset
-	daylight_hour = (ephem.localtime(o.next_rising(s)).hour + 1) + UTC_offset # Give an extra hour for daylight
+	daylight_hour = (ephem.localtime(o.next_rising(s)).hour + 2) + UTC_offset # Give an extra hour for daylight
 	condition1 = (os.system("ping -c 1 192.168.1.4")==0)
-	condition2 = (current_hour>sunset_hour)or(current_hour<sleep_hour)
-	condition3 = (current_hour>wake_hour)or(current_hour<daylight_hour)
+	condition2 = (current_hour>sunset_hour)and(current_hour<sleep_hour)
+	condition3 = (current_hour>wake_hour)and(current_hour<daylight_hour)
 	if condition1 and (condition2 or condition3):
 		print "Switch on"
 		#en.switch_on()
