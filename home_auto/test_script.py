3ce5f711d0d665ad4accde2bf44a4d54e1b1deb8
kel85uk <kel85uk@gmail.com>
2016-03-22 21:55:17 +0000

    Modified home_auto to be more robust

--- home_auto/test_script.py
+++ home_auto/test_script.py
@@ -2,6 +2,7 @@ import energenie as en
 import time
 import os
 import ephem
+import numpy as np
 
 o = ephem.Observer()
 # Latitude and longitude of the observer (This case it is Delft)
@@ -19,13 +20,18 @@ while True:
 	sunset_hour = ephem.localtime(o.next_setting(s)).hour + UTC_offset
 	daylight_hour = (ephem.localtime(o.next_rising(s)).hour + 2) + UTC_offset # Give an extra hour for daylight
 	condition1 = (os.system("ping -c 1 192.168.1.4")==0)
-	condition2 = (current_hour>sunset_hour)and(current_hour<sleep_hour)
-	condition3 = (current_hour>wake_hour)and(current_hour<daylight_hour)
-	if condition1 and (condition2 or condition3):
+	conditionOS = list()
+	for ii in range(0,10):
+		time.sleep(5)
+		conditionOS.append(os.system("ping -c 1 192.168.1.4")==0)
+	condOS = np.sum(conditionOS)
+	condition2 = (current_hour>=sunset_hour)and(current_hour<=sleep_hour)
+	condition3 = (current_hour>=wake_hour)and(current_hour<=daylight_hour)
+	if (condition1 or (condOS > 0)) and (condition2 or condition3):
 		print "Switch on"
-		#en.switch_on()
+		en.switch_on()
 	else:
 		print "Switch off"
-		#en.switch_off()
+		en.switch_off()
 	time.sleep(30)
 	s.compute()
--- home_auto/testshell.sh
+++ home_auto/testshell.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sleep 10
-sudo python /home/pi/Projects/home_auto/test_script.py
+#sudo python /home/pi/Projects/home_auto/test_script.py
